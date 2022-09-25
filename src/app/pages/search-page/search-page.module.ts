0ffe69fa55f3bc57bab9249136071a5daec86ec5
Carlos Nassif <costa.carlos12@gmail.com>
2022-09-25 20:40:45 -0300

    style [test-1]: Add basic style to main page

---
+++ src/app/pages/search-page/search-page.module.ts
@@ -0,0 +1,10 @@
+import { SearchPageComponent } from './search-page.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [SearchPageComponent],
+  imports: [CommonModule],
+  exports: [SearchPageComponent],
+})
+export class SearchPageModule {}
