30431897ff30da9809ecec544977bf48af4997b5
Carlos Nassif <costa.carlos12@gmail.com>
2022-09-26 20:01:03 -0300

    feat [test-1]: Add cache to ChuckNorrisService

--- src/app/services/chuck-norris/chuck-norris.service.ts
+++ src/app/services/chuck-norris/chuck-norris.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 
 import { JokesResponse } from '../../models/JokesResponse';
 
@@ -8,10 +8,24 @@ import { JokesResponse } from '../../models/JokesResponse';
   providedIn: 'root',
 })
 export class ChuckNorrisService {
+  private cache: {
+    [query: string]: JokesResponse;
+  } = {};
+
   constructor(private http: HttpClient) {}
 
   getJokes(query: string): Observable<JokesResponse> {
+    if (!!this.cache[query]) {
+      return of(this.cache[query]);
+    }
+
     const URL: string = `https://api.chucknorris.io/jokes/search?query=${query}`;
-    return this.http.get<JokesResponse>(URL);
+    return this.http.get<JokesResponse>(URL).pipe(this.cacheQuery(query));
+  }
+
+  private cacheQuery(query: string) {
+    return tap((e: JokesResponse) => {
+      this.cache[query] = e;
+    });
   }
 }
