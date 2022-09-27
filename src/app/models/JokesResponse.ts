c3f691c7fbbf0f7818f6c01c65f510eda0a5dd54
Carlos Nassif <costa.carlos12@gmail.com>
2022-09-26 21:17:11 -0300

    feat [test-1]: Add more info on the jokes results list

--- src/app/models/JokesResponse.ts
+++ src/app/models/JokesResponse.ts
@@ -4,11 +4,30 @@ export interface JokesResponse {
 }
 
 export interface Joke {
-  categories: string[];
+  categories: Category[];
   created_at: string;
   icon_url: string;
   id: string;
   updated_at: string;
   url: string;
   value: string;
+}
+
+export enum Category {
+  Animal = 'animal',
+  Career = 'career',
+  Celebrity = 'celebrity',
+  Dev = 'dev',
+  Explicit = 'explicit',
+  Fashion = 'fashion',
+  Food = 'food',
+  History = 'history',
+  Money = 'money',
+  Movie = 'movie',
+  Music = 'music',
+  Political = 'political',
+  Religion = 'religion',
+  Science = 'science',
+  Sport = 'sport',
+  Travel = 'travel',
 }
