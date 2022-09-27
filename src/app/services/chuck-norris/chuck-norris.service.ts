0c465516845391ff423a6df7b7b1943f92be8b30
Carlos Nassif <costa.carlos12@gmail.com>
2022-09-26 21:52:39 -0300

    docs [test-1]: Add basic documentation

--- src/app/services/chuck-norris/chuck-norris.service.ts
+++ src/app/services/chuck-norris/chuck-norris.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of, tap } from 'rxjs';
+import { MonoTypeOperatorFunction, Observable, of, tap } from 'rxjs';
 
 import { JokesResponse } from '../../models/JokesResponse';
 
@@ -14,6 +14,13 @@ export class ChuckNorrisService {
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * Searches on the ChuckNorris.io API for the query given and prevents requests
+   * for the same query.
+   *
+   * @param query full-text to be search
+   * @returns list of jokes that contains full-text searched
+   */
   getJokes(query: string): Observable<JokesResponse> {
     if (!!this.cache[query]) {
       return of(this.cache[query]);
@@ -23,7 +30,14 @@ export class ChuckNorrisService {
     return this.http.get<JokesResponse>(URL).pipe(this.cacheQuery(query));
   }
 
-  private cacheQuery(query: string) {
+  /**
+   * Stores the results of given query on the cache object. The key used is the
+   * query value itself.
+   *
+   * @param query
+   * @returns function to store the query's result Jokes on the cache
+   */
+  private cacheQuery(query: string): MonoTypeOperatorFunction<JokesResponse> {
     return tap((e: JokesResponse) => {
       this.cache[query] = e;
     });
